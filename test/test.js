b270e396d347ac4168f48e2a3fec176ff5e46d86
Lucas Bledsoe <lucas.bledsoe@adyen.com>
2017-09-12 07:42:49 -0700

    clearedout file history with credentials

---
+++ test/test.js
@@ -0,0 +1,204 @@
+'use strict';
+const Adyen = require('../lib/adyen');
+const AdyenErrors = require('../lib/Errors');
+const config = require('./config.js');
+const AdyenUtil = require('../lib/AdyenUtil');
+
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+const mocha = require('mocha');
+const expect = chai.expect;
+const describe = mocha.describe;
+const it = mocha.it;
+chai.use(chaiAsPromised);
+
+
+
+
+
+describe('Create Adyen Object', ()=>{
+  describe('No init', ()=>{
+    const ady = new Adyen();
+    it('raise APIAuthenticationError when performing auth',()=>{
+      // ady.Payment.authorise(config.BASIC_CARD_AUTH).catch((err)=>{console.log(err);});
+      return expect(ady.Payment.authorise(config.BASIC_CARD_AUTH)).to.eventually.be.rejectedWith(AdyenErrors.APIAuthenticationError);
+    });
+    it('set user and password after creation',()=>{
+      ady.user = config.user;
+      expect(ady.client.user).to.equal(config.user);
+      ady.password = config.password;
+      expect(ady.client.password).to.equal(config.password);
+    });
+    it('No APIAuthenticationError raised on auth request after supplying credentials',()=>{
+      return expect(ady.Payment.authorise(config.BASIC_CARD_AUTH)).to.eventually.not.be.an.instanceOf(AdyenErrors.APIAuthenticationError);
+    });
+  });
+
+  describe('Provide credentials on init', ()=>{
+    const ady = new Adyen({user:config.user,password:config.password});
+    it('set user and password set from initializing',()=>{
+      expect(ady.client.user).to.equal(config.user);
+      expect(ady.client.password).to.equal(config.password);
+    });
+    it('No APIAuthenticationError raised on auth request',()=>{
+      return expect(ady.Payment.authorise(config.BASIC_CARD_AUTH)).to.eventually.not.be.an.instanceOf(AdyenErrors.APIAuthenticationError);
+    });
+  });
+
+  describe('set merchantAccount in adyen object',()=>{
+    const ady = new Adyen({merchantAccount:config.merchantAccount});
+    expect(ady.merchantAccount).to.equal(config.merchantAccount);
+  });
+});
+
+describe('Intentional Errors', ()=>{
+  var ady = new Adyen(config.ADYEN_CONFIG);
+  const BASIC_CARD_AUTH_NO_AMOUNT = {
+    card:{
+      number:"4111 1111 1111 1111",
+      holderName:"Test Name",
+      expiryMonth:"10",
+      expiryYear:"2020",
+      cvc:"737"
+    },
+    reference:"testreference"
+  };
+  const BASIC_CARD_AUTH_NO_REF = {
+    amount:{
+      value:100,
+      currency:"EUR"
+    },
+    card:{
+      number:"4111 1111 1111 1111",
+      holderName:"Test Name",
+      expiryMonth:"10",
+      expiryYear:"2020",
+      cvc:"737"
+    }
+  };
+  describe("InvalidRequestError on auth", () =>{
+    it('when missing amount on auth',()=>{
+      return expect(ady.Payment.authorise(BASIC_CARD_AUTH_NO_AMOUNT)).to.eventually.be.rejectedWith(AdyenErrors.InvalidRequestError);
+    });
+    it('when missing reference',()=>{
+      return expect(ady.Payment.authorise(BASIC_CARD_AUTH_NO_REF)).to.eventually.be.rejectedWith(AdyenErrors.InvalidRequestError);
+    });
+  });
+});
+
+describe('Payment Service', ()=>{
+  var authResult = {};
+  const ady = new Adyen(config.ADYEN_CONFIG);
+  describe('authorise',()=>{
+    const CARD_AUTH_INIT_RECUR = Object.assign({}, config.BASIC_CARD_AUTH,{
+      shopperReference:config.SHOPPER_REFERENCE,
+      recurring:{contract:"RECURRING"}
+    });
+    it('pspreference on succesful auth',()=>{
+      return ady.Payment.authorise(CARD_AUTH_INIT_RECUR)
+      .then(res =>{
+        Object.assign(authResult, res);
+        expect(authResult).to.have.property('pspReference');
+        expect(authResult).to.have.property('resultCode','Authorised');
+      });
+    });
+  });
+  describe('cancel',()=>{
+    it('[cancel-received] on cancel',()=>{
+      return expect(ady.Payment.cancel({originalReference:authResult.pspReference}))
+      .to.eventually.have.property('response','[cancel-received]');
+    });
+  });
+  describe('refund',()=>{
+    it('[refund-received] on refund',()=>{
+      return expect(ady.Payment.refund({
+        originalReference:authResult.pspReference,
+        modificationAmount:{
+          value:100,
+          currency:"EUR"
+        }
+      })).to.eventually.have.property('response','[refund-received]');
+    });
+  });
+  describe('capture',()=>{
+    it('[capture-received] on capture',()=>{
+      return expect(ady.Payment.capture({
+        originalReference:authResult.pspReference,
+        modificationAmount:{
+          value:100,
+          currency:"EUR"
+        }
+      })).to.eventually.have.property('response','[capture-received]');
+    });
+  });
+  describe('cancelOrRefund',()=>{
+    it('[cancelOrRefund-received] on cancelOrRefund',()=>{
+      return expect(ady.Payment.cancelOrRefund({originalReference:authResult.pspReference}))
+      .to.eventually.have.property('response','[cancelOrRefund-received]');
+    });
+  });
+  describe('refundWithData',()=>{
+    it('pspreference on succesful refundWithData',()=>{
+      return ady.Payment.refundWithData(config.BASIC_CARD_AUTH)
+      .then(res =>{
+        Object.assign(authResult, res);
+        expect(authResult).to.have.property('pspReference');
+      });
+    });
+  });
+});
+
+describe('HPP Service', () => {
+  const ady = new Adyen(config.ADYEN_CONFIG);
+  describe('generate256HMAC', () => {
+    it('generate accurate hmac matching 4xaTVPW+C0tOErcNsz/8cblg0JVxPYZzqtAJ0LIYsUI=', ()=>{
+      const request = {
+        countryCode : 'US',
+        currencyCode : 'EUR',
+        merchantReference : 'TEST-PAYMENT',
+        paymentAmount : '199',
+        sessionValidity : '2100-01-01T00:00:00+00:00',
+        shopperLocale : 'en_US',
+        merchantAccount : config.merchantAccount,
+        skinCode: config.skinCode
+      };
+      expect(AdyenUtil.generate256HMAC(request, config.hmacKey)).to.equal("4xaTVPW+C0tOErcNsz/8cblg0JVxPYZzqtAJ0LIYsUI=");
+    });
+  });
+  describe('directoryLookup', () => {
+    it('return list of valid payment methods', ()=>{
+      const request = {
+        countryCode : 'US',
+        currencyCode : 'EUR',
+        merchantReference : 'TEST-PAYMENT',
+        paymentAmount : '199',
+        sessionValidity : '2100-01-01T00:00:00+00:00',
+        shopperLocale : 'en_US'
+      };
+      return expect(ady.HPP.directoryLookup(request)).to.eventually.have.property("paymentMethods");
+    });
+  });
+});
+
+describe('Recurring Service',()=>{
+  const ady = new Adyen(config.ADYEN_CONFIG);
+  describe('listRecurringDetails',()=>{
+    it('return minimal object when invalid shopperReference',()=>{
+      return expect(ady.Recurring.listRecurringDetails({shopperReference:'nonexistantshopperReference'}))
+        .to.eventually.have.property('invalidOneclickContracts','false');
+    });
+
+    it('return details when shopperReference present',()=>{
+      return ady.Recurring.listRecurringDetails({shopperReference:config.SHOPPER_REFERENCE})
+      .then(adyenResult=>{
+        return expect(adyenResult).have.property('details');
+      });
+    });
+  });
+  describe('disable',()=>{
+    it('return [all-details-successfully-disabled] on succesful disable',()=>{
+      return expect(ady.Recurring.disable({shopperReference:config.SHOPPER_REFERENCE}))
+        .to.eventually.have.property('response', '[all-details-successfully-disabled]');
+    });
+  });
+});
